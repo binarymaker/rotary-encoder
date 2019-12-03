da108c8e6203c7d662835e112ea88bb37fb2b40a
binarymaker <importfunfromcode@gmail.com>
2019-12-03 16:42:21 +0530

    project directory included

---
+++ config/rotary-encoder-cfg.h
@@ -0,0 +1,40 @@
+/**\cond
+  ******************************************************************************
+  * ______  _                             ___  ___        _               
+  * | ___ \(_)                            |  \/  |       | |              
+  * | |_/ / _  _ __    __ _  _ __  _   _  | .  . |  __ _ | | __ ___  _ __ 
+  * | ___ \| || '_ \  / _` || '__|| | | | | |\/| | / _` || |/ // _ \| '__|
+  * | |_/ /| || | | || (_| || |   | |_| | | |  | || (_| ||   <|  __/| |   
+  * \____/ |_||_| |_| \__,_||_|    \__, | \_|  |_/ \__,_||_|\_\\___||_|   
+  *                                 __/ |                                 
+  *                                |___/                                  
+  *                                                                       
+  * Copyright (C) 2019 Binary Maker - All Rights Reserved
+  *
+  * This program and the accompanying materials are made available
+  * under the terms described in the LICENSE file which accompanies
+  * this distribution.
+  * Written by Binary Maker <https://github.com/binarymaker>
+  ******************************************************************************
+  \endcond*/
+
+#ifndef ROTARY_ENCODER_458ebb00_15b9_11ea_bd8d_705a0f25cb51
+#define ROTARY_ENCODER_458ebb00_15b9_11ea_bd8d_705a0f25cb51
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions ------------------------------------------------------- */
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ROTARY_ENCODER_458ebb00_15b9_11ea_bd8d_705a0f25cb51 */
+
